3708d38fe5ea15872d897b1aeadb27ed9e53b6a1
sheldonu <uch20002@byui.edu>
2024-02-13 12:04:56 -0700

    validation updated

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,28 +2,19 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find()
-  .toArray((err, lists) => {
-    if (err) {
-      res.status(400).json({ message: err });
-    }
+  const result = await mongodb.getDb().db().collection('contacts').find();
+  result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
 };
 
 const getSingle = async (req, res, next) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to find a contact.');
-  }
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId })
-  .toArray((err, result) => {
-    if (err) {
-      res.status(400).json({ message: err });
-    }
+  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+  result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result[0]);
+    res.status(200).json(lists[0]);
   });
 };
 
@@ -44,9 +35,6 @@ const createContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to update a contact.');
-  }
   const userId = new ObjectId(req.params.id);
   // be aware of updateOne if you only want to update specific fields
   const contact = {
@@ -66,9 +54,6 @@ const updateContact = async (req, res) => {
 };
 
 const deleteContact = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to delete a contact.');
-  }
   const userId = new ObjectId(req.params.id);
   const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId }, true);
   console.log(response);
@@ -80,11 +65,8 @@ const deleteContact = async (req, res) => {
 };
 
 const getAllBooks = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('books').find()
-  .toArray((err, lists) => {
-    if (err) {
-      res.status(400).json({ message: err });
-    }
+  const result = await mongodb.getDb().db().collection('books').find();
+  result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
@@ -92,13 +74,10 @@ const getAllBooks = async (req, res, next) => {
 
 const getSingleBooks = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('books').find({ _id: userId })
-  .toArray((err, result) => {
-    if (err) {
-      res.status(400).json({ message: err });
-    }
+  const result = await mongodb.getDb().db().collection('books').find({ _id: userId });
+  result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result[0]);
+    res.status(200).json(lists[0]);
   });
 };
 
--- helpers/validate.js
+++ helpers/validate.js
@@ -1,4 +1,4 @@
-const Validator = require('validatorjs');
+let Validator = require('validatorjs');
 const validator = (body, rules, customMessages, callback) => {
     const validation = new Validator(body, rules, customMessages);
     validation.passes(() => callback(null, true));
